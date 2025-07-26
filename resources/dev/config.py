ace1cbf8418fed9e74c9c9539c136c2239acbe43
Shubham Wagh <shrishubhamwagh@gmail.com>
2025-07-27 02:03:34 +0300

    assignment

---
+++ resources/dev/config.py
@@ -0,0 +1,50 @@
+# from dotenv import load_dotenv
+import os
+
+# load_dotenv()
+
+REDDIT_CLIENT_ID = "TJ7uJwaZBzXFwWRPdyEMAw"
+REDDIT_CLIENT_SECRET = "CCo92jLQv0QUQiRWAxpytePTtDG4Vw"
+GOOGLE_API_KEY = "AIzaSyAyju_Pc5_y0rF9M8BURFz7h9P9C0L9B_0"
+GEMINI_ENDPOINT = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
+USER_AGENT = "academic_research_v1 by u/Active"
+TARGET_SUBREDDITS = ["conspiracy", "politics", "askconspiracy"]
+SEARCH_TERMS = ["jew", "zionist", "holocaust denial", "antisemitic"]
+DATA_DAYS_LOOKBACK = 60
+
+
+# export REDDIT_CLIENT_ID="TJ7uJwaZBzXFwWRPdyEMAw"
+# export REDDIT_CLIENT_SECRET="CCo92jLQv0QUQiRWAxpytePTtDG4Vw"
+# export USER_AGENT="my_user_agent"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import os
+# import sys
+
+# #Reddit Access And Secret key
+
+# client_id="TJ7uJwaZBzXFwWRPdyEMAw"
+# client_secret="CCo92jLQv0QUQiRWAxpytePTtDG4Vw"
+# harmful_posts='/home/swa316/harmful_posts.csv'
+# problematic_users='/home/swa316/problematic_users.csv'
+# GOOGLE_API_KEY ="AIzaSyAyju_Pc5_y0rF9M8BURFz7h9P9C0L9B_0"
+# GEMINI_ENDPOINT=f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
+# # File Download location
+
+
+# # read path
